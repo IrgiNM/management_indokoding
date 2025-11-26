59988f67f7bd8d9eccc7a4d476873618cc0f619e
unknown <irginazwamustofa@gmail.com>
2025-11-26 16:59:57 +0700

    update sudah bisa cek token udah beres

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { getUserId } from '@/hooks/api';
 import { getToken } from '@/hooks/tokenFunction';
 import { useRouter } from 'expo-router';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 
 const index = () => {
@@ -12,16 +12,18 @@ const index = () => {
     setLoading(true)
     try{
       const token = await getToken();
-      if(token){
+      if(token !== ''){
+        // console.error('sebelum data')
         const data = await getUserId();
-        if(data){
+        // console.error('habis data');
+        if(data.status===200){
           return router.replace('/(tabs)/home');
-        }else{
-          return router.replace('/login');
-        }
+        } 
+       
       }
     }catch{
-      console.error('gagal login')
+      // console.error('gagal login')
+      return router.replace('/login');
     }finally{
       setLoading(false)
     }
@@ -33,9 +35,11 @@ const index = () => {
 
   return (
     <View className='flex-1 justify-center items-center'>
+      {loading&&
         <Text className='font-bold text-[10px] text-black'>
-          {loading&&'loading...'}
+          loading...
         </Text>
+      }
     </View>
   )
 }
